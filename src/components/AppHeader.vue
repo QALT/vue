5a79b7b58f24ba6a92d542f82a987b8a06e09447
Amin NAIRI <nairi.amin@gmail.com>
2020-11-18 12:27:26 +0100

    :sparkles: added logout button

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,7 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">Navbar</a>
+
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -8,20 +9,30 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    <router-link class="nav-link" to="/">Home</router-link>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <router-link class="nav-link" to="/login">Login</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link class="nav-link" to="/register">Register</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link class="nav-link" to="/about">About</router-link>
+                </li>
+                <li class="nav-item">
+                    <b-button @click="logout">Logout</b-button>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
+                        Dropdown
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
+                        <router-link to="/">Home</router-link>
+                        <router-link to="/login">Login</router-link>
+                        <router-link to="/register">Register</router-link>
+                        <router-link to="/">Home</router-link>
+                        <router-link to="/">Home</router-link>
                     </div>
                 </li>
                 <li class="nav-item">
@@ -35,9 +46,20 @@
 
 <script>
 import ApiSwitch from './ApiSwitch.vue'
+import {mapMutations} from "vuex";
 
 export default {
     name: "AppHeader",
     components: { ApiSwitch },
+
+    methods: {
+        ...mapMutations([
+            "setToken"
+        ]),
+        logout() {
+            this.setToken(null);
+            this.$router.push("/login");
+        }
+    }
 }
 </script>
