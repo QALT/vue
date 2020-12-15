d702d0ce6b66b59980e48bd716acc1d24552a4b3
Lucas Lavander <xz351@hotmail.fr>
2020-12-15 20:00:23 +0100

    Copy the api switch to the top nav

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,6 @@
 <template>
     <b-navbar variant="light" class="topbar mb-4 static-top shadow">
+        <api-switch class="pt-3 pb-3"></api-switch>
         <account-view></account-view>
 	</b-navbar>
 
@@ -7,10 +8,13 @@
 
 <script>
 import AccountView from './AccountView';
+import ApiSwitch from './ApiSwitch';
+
 export default {
     name: "AppHeader",
     components: {
-        AccountView
+        AccountView,
+        ApiSwitch
     }
 }
 </script>
