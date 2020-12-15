a4ecd7e8f7d48b3f8467dec7e1cadb50cec5f761
Lucas Lavander <xz351@hotmail.fr>
2020-12-15 20:01:00 +0100

    Fix axios token refresh

--- src/services/api-platform/httpClient.js
+++ src/services/api-platform/httpClient.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 import store from '../../store';
 
-const httpClient = () => {
-    return axios.create({
-        baseURL: process.env.VUE_APP_API_PLATFORM_URL || 'https://jsonplaceholder.typicode.com/',
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${store.getters.getToken}`
-            // anything you want to add to the headers
-        }
-    });
-}
+export const httpClient = axios.create({
+    baseURL: process.env.VUE_APP_API_PLATFORM_URL,
+    headers: {
+        "Content-Type": "application/json"
+    }
+});
 
-export default httpClient();
+httpClient.interceptors.request.use(function (config) {
+    const authKey = store.getters.getToken
+    config.headers["Authorization"] = "Bearer " + authKey;
+    return config;
+});
