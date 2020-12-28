e139d709dde1d9838c9d3e01daaa9a9aa080d97f
Lucas Lavander <xz351@hotmail.fr>
2020-12-28 18:14:49 +0100

    Fix usage of env variable

--- src/services/prisma/apolloClient.js
+++ src/services/prisma/apolloClient.js
@@ -2,7 +2,7 @@ import ApolloClient from 'apollo-boost'
 import store from '../../store';
 
 export const apolloClient = new ApolloClient({
-    uri: process.env.VUE_APP_GRAPHQL_HTTP,
+    uri: process.env.VUE_APP_GRAPHQL_URL,
     headers: {
       authorization: `Bearer ${store.getters.getToken}`
     }
