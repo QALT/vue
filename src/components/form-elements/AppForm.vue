2e8b83c9612913422a844b7279d68269c853bb87
Lucas Lavander <xz351@hotmail.fr>
2020-12-28 18:25:12 +0100

    Implement form components

---
+++ src/components/form-elements/AppForm.vue
@@ -0,0 +1,37 @@
+<template>
+    <form v-on="$listeners" v-bind="$attrs" @submit="submitForm">
+        <slot />
+    </form>
+</template>
+
+<script>
+export default {
+    props: {
+        values: Object
+    },
+
+    methods: {
+        submit() {},
+
+        submitForm(event) {
+            event.preventDefault();
+            this.submit();
+        }
+    },
+
+    data() {
+        return {
+            formValues: this.values
+        };
+    },
+
+    provide() {
+        const {submit, formValues: values} = this;
+
+        return {
+            submit,
+            values
+        };
+    }
+}
+</script>
