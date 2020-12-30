f1899ed39f94347f16d100c54c89852f08ac2149
Lucas Lavander <xz351@hotmail.fr>
2020-12-30 21:28:08 +0100

    Fix apollo client token

--- src/services/prisma/apolloClient.js
+++ src/services/prisma/apolloClient.js
@@ -1,9 +1,15 @@
 import ApolloClient from 'apollo-boost'
 import store from '../../store';
 
+
 export const apolloClient = new ApolloClient({
-    uri: process.env.VUE_APP_GRAPHQL_URL,
-    headers: {
-      authorization: `Bearer ${store.getters.getToken}`
-    }
-  })
+  uri: process.env.VUE_APP_GRAPHQL_URL,
+  request: (operation) => {
+    const token = store.getters.getToken;
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  }
+});
